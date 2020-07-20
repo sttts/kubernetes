p0lyn0mial/origin

UPSTREAM: <drop>: skip PSP test that is inconsistently being skipped

test/e2e/auth/pod_security_policy.go
@@ -52,6 +52,7 @@ var _ = SIGDescribe("PodSecurityPolicy", func() {
 	var c clientset.Interface
 	var ns string // Test namespace, for convenience
 	ginkgo.BeforeEach(func() {
+		framework.Skipf("This test should always be skipped for us, but occasionally PSP is on!")
 		if !framework.IsPodSecurityPolicyEnabled(f.ClientSet) {
 			framework.Skipf("PodSecurityPolicy not enabled")
 		}

sttts/kubernetes

Origin-commit: 131dbb4770bb3bed0c07d2a6ca0cbe4cba2556bb

test/e2e/auth/pod_security_policy.go
@@ -52,6 +52,7 @@ var _ = SIGDescribe("PodSecurityPolicy [Feature:PodSecurityPolicy]", func() {
 	var c clientset.Interface
 	var ns string // Test namespace, for convenience
 	ginkgo.BeforeEach(func() {
+		e2eskipper.Skipf("This test should always be skipped for us, but occasionally PSP is on!")
 		if !framework.IsPodSecurityPolicyEnabled(f.ClientSet) {
 			framework.Failf("PodSecurityPolicy not enabled")
 			return