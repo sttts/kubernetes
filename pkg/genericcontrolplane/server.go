e36f7cc114950e732584ff60ea8e8abd15672ee3
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-02-04 13:50:20 +0100

    genericcontrolplane: restore flags and validation

--- pkg/genericcontrolplane/server.go
+++ pkg/genericcontrolplane/server.go
@@ -91,7 +91,7 @@ func Run(options options.CompletedServerRunOptions, stopCh <-chan struct{}) erro
 	apiExtensionsConfig, err := CreateAPIExtensionsConfig(
 		*config.GenericConfig,
 		config.ExtraConfig.VersionedInformers,
-		nil,
+		nil, // pluginInitializer
 		options,
 		&unimplementedServiceResolver{},
 		webhook.NewDefaultAuthenticationInfoResolverWrapper(
@@ -205,7 +205,7 @@ func CreateKubeAPIServerConfig(
 		return nil, err
 	}
 
-	// // Load the public keys.g
+	// // Load the public keys.
 	// var pubKeys []interface{}
 	// for _, f := range s.Authentication.ServiceAccounts.KeyFiles {
 	// 	keys, err := keyutil.PublicKeysFromFile(f)
@@ -317,9 +317,10 @@ func BuildGenericConfig(
 		return
 	}
 
-	//if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.APIPriorityAndFairness) && s.GenericServerRunOptions.EnablePriorityAndFairness {
-	//	genericConfig.FlowControl, lastErr = BuildPriorityAndFairness(s, clientgoExternalClient, versionedInformers)
-	//}
+	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.APIPriorityAndFairness) && o.GenericServerRunOptions.EnablePriorityAndFairness {
+		// TODO(sttts): make BuildPriorityAndFairness take the completed options
+		genericConfig.FlowControl, lastErr = BuildPriorityAndFairness(&o.ServerRunOptions, clientgoExternalClient, versionedInformers)
+	}
 
 	return
 }
