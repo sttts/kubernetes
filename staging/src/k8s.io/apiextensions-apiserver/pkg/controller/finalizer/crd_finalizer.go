2a8aebe9f7799aa798bbca04ffc1385b19fd7b1e
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-12-05 14:53:54 +0100

    SQUASH: /apiextensions-apiserver

--- staging/src/k8s.io/apiextensions-apiserver/pkg/controller/finalizer/crd_finalizer.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/controller/finalizer/crd_finalizer.go
@@ -195,7 +195,7 @@ func (c *CRDFinalizer) deleteInstances(crd *apiextensionsv1.CustomResourceDefini
 	}
 
 	ctx := genericapirequest.WithCluster(genericapirequest.NewContext(), genericapirequest.Cluster{
-		Name: logicalcluster.From(crd).Path(),
+		Name: logicalcluster.From(crd),
 	})
 
 	allResources, err := crClient.List(ctx, nil)
