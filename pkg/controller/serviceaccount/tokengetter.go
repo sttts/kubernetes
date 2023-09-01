0f6f006c086a4bc15ed94727d5fe68bd1b18fa46
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-09-01 12:14:15 +0200

    UPSTREAM: <carry>: clusterize controllers
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
    Co-authored-by: Marvin Beckers <marvin@kubermatic.com>
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/controller/serviceaccount/tokengetter.go
+++ pkg/controller/serviceaccount/tokengetter.go
@@ -14,17 +14,48 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +kcp-code-generator:skip
+
 package serviceaccount
 
 import (
 	"context"
-	"k8s.io/api/core/v1"
+
+	kcpkubernetesclientset "github.com/kcp-dev/client-go/kubernetes"
+	kcpcorev1listers "github.com/kcp-dev/client-go/listers/core/v1"
+	"github.com/kcp-dev/logicalcluster/v3"
+
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	v1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/kubernetes/pkg/serviceaccount"
 )
+
+func NewClusterGetterFromClient(client kcpkubernetesclientset.ClusterInterface, secretLister kcpcorev1listers.SecretClusterLister, serviceAccountLister kcpcorev1listers.ServiceAccountClusterLister /*podLister kcpcorev1listers.PodClusterLister*/) serviceaccount.ServiceAccountTokenClusterGetter {
+	return &serviceAccountTokenClusterGetter{
+		client:               client,
+		secretLister:         secretLister,
+		serviceAccountLister: serviceAccountLister,
+	}
+}
+
+type serviceAccountTokenClusterGetter struct {
+	client               kcpkubernetesclientset.ClusterInterface
+	secretLister         kcpcorev1listers.SecretClusterLister
+	serviceAccountLister kcpcorev1listers.ServiceAccountClusterLister
+	podLister            kcpcorev1listers.PodClusterLister
+}
+
+func (s *serviceAccountTokenClusterGetter) Cluster(name logicalcluster.Name) serviceaccount.ServiceAccountTokenGetter {
+	return NewGetterFromClient(
+		s.client.Cluster(name.Path()),
+		s.secretLister.Cluster(name),
+		s.serviceAccountLister.Cluster(name),
+		nil,
+	)
+}
 
 // clientGetter implements ServiceAccountTokenGetter using a clientset.Interface
 type clientGetter struct {
@@ -51,9 +82,6 @@ func (c clientGetter) GetServiceAccount(namespace, name string) (*v1.ServiceAcco
 }
 
 func (c clientGetter) GetPod(namespace, name string) (*v1.Pod, error) {
-	if pod, err := c.podLister.Pods(namespace).Get(name); err == nil {
-		return pod, nil
-	}
 	return c.client.CoreV1().Pods(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 }
 
