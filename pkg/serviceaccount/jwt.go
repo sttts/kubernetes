f39deeba91feb6688fe377b82dfb1ba1bd9e1bd5
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-09-11 09:44:07 +0200

    SQUASH: auth
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- pkg/serviceaccount/jwt.go
+++ pkg/serviceaccount/jwt.go
@@ -30,8 +30,8 @@ import (
 
 	"github.com/kcp-dev/logicalcluster/v3"
 	"gopkg.in/square/go-jose.v2"
-	jose "gopkg.in/square/go-jose.v2"
 	"gopkg.in/square/go-jose.v2/jwt"
+
 	v1 "k8s.io/api/core/v1"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apiserver/pkg/audit"
